80f50da524a1b17fab6452f453747cff583149b6
Andre Corazim <andrecorazim@gmail.com>
2019-12-29 04:13:07 -0300

    Geral

--- man/header.php
+++ man/header.php
@@ -7,19 +7,19 @@ class Headers
     {
 
         $navBar = <<< EOT
-    <header>
-        <img class="logo" src="../image/logo.png" alt="logo">
-        <nav>
-            <ul class="nav_links">
-                <li><a href="manager.php">Dashboard</a></li>
-                <li><a href="manager.usuarios.php">Usuarios</a></li>
-                <li><a href="#">Produtos</a></li>
-                <li><a href="#">Produtos</a
-                li>
-            </ul>
-        </nav>
-        <a class="cta iconnavbar" href="#"><i class="fas fa-user-circle"></i></a>
-    </header>   
+        <header>
+            <img class="logo" src="../image/logo.png" alt="logo">
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="manager.php">Dashboard</a></li>
+                    <li><a href="manager.usuarios.php">Usuarios</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Produtos</a
+                    li>
+                </ul>
+            </nav>
+            <a class="cta iconnavbar" href="#"><i class="fas fa-user-circle"></i></a>
+        </header>   
 
 EOT;
 
