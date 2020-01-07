b50f0433e99ae7c6e2d02d97663db09b5837c6f5
André Corazim <andre@systemhope.com.br>
2020-01-07 07:43:29 -0300

    Geral

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 
-//ini_set('memory_limit', '256M');
+/*//ini_set('memory_limit', '256M');
 ini_set('display_errors', 1);
 ini_set('display_startup_erros', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 include('class/class.UserPermission.php');
 
