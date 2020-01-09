2acd92a5dbd0d047bbfd3bc47a7f8c28b6889389
Paulo Corazim <paulocorazim22@gmail.com>
2020-01-09 18:24:19 -0300

    Informação em cima 2

--- class/class.ScreenUsers.php
+++ class/class.ScreenUsers.php
@@ -7,11 +7,13 @@ class ScreenUsers
         $screenFormUser = <<< EOT
         <div class="containerprincipal">
             <div class="margin_esq_padrao">
-                <h2> Cadastro de Usuário</h2>
+                <h2>Cadastro de Usuário</h2>
             </div>
             <div class="flex_main">
                 <div class="flex_sub">
                     <form action="manager.users.php" method="post">
+                    <h3 class="spaninput">Informações:</h3>
+                    <hr>
                         <div class="input-group">
                             <input type="hidden" name="user_id" id="user_id">
                             <input type="text" name="user_name" id="user_name" class="form-control">
@@ -36,11 +38,13 @@ class ScreenUsers
                         </div>
                 </div>
             <div class="flex_sub">     
-            <h3 class="spaninput">Permissões do Usuário</h3>      
+            <h3 class="spaninput">Permissões do Usuário</h3>  
+            <hr>    
                 <div class="input-group">
                     <input type="checkbox" name="permission_master" id="permission_master" text="master" value="master"><span>Master ( Permissão ADMIN )</span>
                 </div>  
-                <h3 class="spaninput">Módulos que o usuário pode acessar:</h3>                
+                <h3 class="spaninput">Módulos que o usuário pode acessar:</h3>  
+                <hr>              
                 <div class="input-group">                        
                    <select name="user_module[]" id="user_module[]" multiple>
                $typeModules[1]
@@ -48,16 +52,20 @@ class ScreenUsers
                 </div>        
             </div>
             <div class="flex_sub">
-                    <span class="spaninput">Permissões para os módulos.</span>
+                    <h3 class="spaninput">Permissões para os módulos.</h3>
+                    <hr>
                     <select name="user_permission[]" id="user_permission[]" multiple style="width:390px">
                         <option value="I"><span>Inserir -> ( Permite inclusão de registros )</span></option>
                         <option value="S"><span>Seleção -> ( Permite seleções de registros )</span></option>
                         <option value="U"><span>Alterar -> ( Permite Alterações de registros )</span></option>
                         <option value="D"><span>APAGAR ->  ( Permite APAGAR registros )</span></option>
-                    </select>
+                    </select><br>
                     <button type="submit" name="btn_update" id="btn_upadte">[ ATUALIZAR ]</button>
                 </form>
             </div>
+        </div>
+        <div class="margin_esq_padrao">
+            <h2> Usuários já Cadastrados</h2>
         </div>
 
 EOT;
