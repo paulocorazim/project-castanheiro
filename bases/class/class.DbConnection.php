d364e17e385bb87b9759b31293253263210c972b
André Corazim <andre@systemhope.com.br>
2020-01-25 01:11:19 -0300

    GRUD de Usuários

--- bases/class/class.DbConnection.php
+++ bases/class/class.DbConnection.php
@@ -5,9 +5,9 @@
 		public function connection ()
 		{
 			require __DIR__ . '/../vendor/autoload.php';
-			$dbInstance = \Simplon\Db\DbInstance::MySQL('iuri0091.hospedagemdesites.ws', 'shcombr_appmanager',
-				'shcombr_app', '}.MC3T@MBG74');
-			//$dbInstance = \Simplon\Db\DbInstance::MySQL('localhost', 'appmanager', 'root', '');
+			$dbInstance = null;
+			//$dbInstance = \Simplon\Db\DbInstance::MySQL('iuri0091.hospedagemdesites.ws', 'shcombr_appmanager','shcombr_app', '}.MC3T@MBG74');
+			$dbInstance = \Simplon\Db\DbInstance::MySQL('localhost', 'appmanager', 'root', '');
 			return $dbInstance;
 		}
 	}
