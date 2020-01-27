3c85b90939e29b172400b427d481e46b2f92db44
Paulo Corazim <paulocorazim22@gmail.com>
2020-01-27 17:28:16 -0300

    rEF.

---
+++ bases/class/class.ScreenBillets.php
@@ -0,0 +1,17 @@
+<?php
+
+class ScreenBillets
+{
+    public function screenFormBillet(){
+
+        $screenFormBillet = <<< EOT
+        
+        <div class="container">OIIII</div>
+
+
+EOT;
+
+    return $screenFormBillet;
+
+    }
+}
