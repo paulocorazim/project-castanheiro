9c50e26fe42b1c11850f102ea439982aad6839c7
Paulo Corazim <paulocorazim22@gmail.com>
2020-01-28 17:43:24 -0300

    billets 3.0

--- bases/class/class.ScreenBillets.php
+++ bases/class/class.ScreenBillets.php
@@ -11,21 +11,45 @@ class ScreenBillets
     <div class="container-fluid"> 
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="p-5">
-              
 
+              <div class="text-left">
+                <h1 class="h3 text-gray-900 mb-3">Incluir Boleto</h1>
+              </div>
+
+              <div class="btn-group mb-3">
+                <button type="button" class="btn btn-primary  dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Ação
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="#">Alguma ação</a>
+                  <a class="dropdown-item" href="#">Outra ação</a>
+                  <a class="dropdown-item" href="#">Alguma coisa aqui</a>
+                </div>
+              </div>
+            
               <div class="form-group row">
-                <div class="col-sm-6 mb-3 mb-sm-0">
-                    <span>Razão Social</span>
-                    <input type="text" name="client_corporate_name" id="client_corporate_name" class="form-control">
+                <div class="col-sm-3 mb-10 mb-sm-0">
+                    <span>Codigo Cliente</span>
+                    <input type="text" name="" id="" class="form-control">
                 </div>
-                <div class="col-sm-6 mb-3 mb-sm-0">
-                    <span>CPF</span>
-                    <input type="text" name="cpfcnpj" id="cpfcnpj" onkeypress='mascaraMutuario(this,cpfCnpj)' onblur='clearTimeout()' class="form-control" placeholder="CPF / CNPJ">
-                </div>
+                <div class="col-sm-3 mb-3 mb-sm-0">
+                  <span>Data Vencimento</span>
+                  <input type="text" name="" id="" class="form-control">
+              </div>
+              <div class="col-sm-3 mb-3 mb-sm-0">
+                <span>Valor</span>
+                <input type="text" name="" id="" class="form-control">
+            </div>
+            <div class="col-sm-3 mb-3 mb-sm-0">
+              <br>
+              <button class="btn btn-primary btn-user btn-block">Enviar</button>
+          </div>
             </div>
 
 
-                <h2 class="h3 mb-2 text-gray-800">Incluir Boleto Avulso</h2>
+            <div class="text-left">
+              <h1 class="h3 text-gray-900 mt-5 mb-5">Incluir Boleto Avulso</h1>
+            </div>
                 <!-- DataTales Example -->
                 <div class="card shadow mb-4">
                   <div class="card-header py-3">
@@ -36,7 +60,7 @@ class ScreenBillets
                       <table cellspacing="0" class="table table-bordered" id="dataTable" width="100%">
                         <thead>
                         <tr>
-                          <th>Cod / Cliente</th>
+                          <th>Codigo Cliente</th>
                           <th>Data Vencimento Original</th>
                           <th>Data Vencimento Prorrogado</th>
                           <th>Custo</th>
@@ -45,7 +69,7 @@ class ScreenBillets
                         </thead>
                         <tfoot>
                           <tr>
-                            <th>Cod / Cliente</th>
+                          <th>Codigo Cliente</th>
                           <th>Data Vencimento Original</th>
                           <th>Data Vencimento Prorrogado</th>
                           <th>Custo</th>
