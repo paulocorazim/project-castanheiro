6cf947cbaebfeb1fe1edd5575debfab6cc929b6b
André Corazim <andre@systemhope.com.br>
2020-01-31 02:56:01 -0300

    Hackathon Castanheiro App

---
+++ bases/class/class.ScreenPlanM2.php
@@ -0,0 +1,171 @@
+<?php
+
+    class ScreenPlanM2s
+    {
+        public function screenReportPlanM2()
+        {
+            return <<< EOT
+            <div class="card shadow mb-4">
+              <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">
+                   Planilha de M2
+                   </h6>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive-sm">
+                  <table cellspacing="0" class="table table-striped table-sm" id="">
+                    <thead>
+                    <tr>
+                      <th>Cód : Cliente</th>
+                      <th>APTO</th>
+                      <th>ÚTIL</th>
+                      <th>TOTAL</th>
+                      <th>LOCATÁRIO</th>
+                      <th>ALUGUEL</th>
+                      <th>M2</th>
+                      <th>REGIÃO</th>
+                      <th>PREVISTO</th>
+                      <th>MÊS</th>
+                      <th>ANO CONTRATO</th>
+                    </tr>
+                    </thead>
+                    <tbody>                 
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    <tr>
+                      <td>1010: SystemHope</td>
+                      <td>n.12134</td>
+                      <td>48,46</td>
+                      <td>69,78</td>
+                      <td>André Corazim</td>
+                      <td>2.200,00</td>
+                      <td>34,99</td>
+                      <td>45,00</td>
+                      <td>2.500,00</td>
+                      <td>Janeiro</td>
+                      <td>2022</td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+EOT;
+        }
+    }
