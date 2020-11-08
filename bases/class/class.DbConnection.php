fbbec80a37ead409cbb9d90317c5a5816bd565ce
Andre Corazim <andrecorazim@gmail.com>
2020-11-08 10:19:41 -0300

    Migraçao do Servidor : Revenda LocaWeb CPanel

--- bases/class/class.DbConnection.php
+++ bases/class/class.DbConnection.php
@@ -6,8 +6,8 @@
         {
             require __DIR__ . '/../vendor/autoload.php';
             $dbInstance = null;
-	        //$dbInstance = \Simplon\Db\DbInstance::MySQL('iuri0091.hospedagemdesites.ws', 'grupocas_castanhe_app', 'grupocas_app', '@14tw07sevi');
-            $dbInstance = \Simplon\Db\DbInstance::MySQL('srv258.prodns.com.br', 'castanhe_app', 'castanhe_app', '@14tw07sevi');
+	        $dbInstance = \Simplon\Db\DbInstance::MySQL('iuri0091.hospedagemdesites.ws', 'grupocas_castanhe_app', 'grupocas_app', '@14TW07sevi');
+            //$dbInstance = \Simplon\Db\DbInstance::MySQL('srv258.prodns.com.br', 'castanhe_app', 'castanhe_app', '@14tw07sevi');
             //$dbInstance = \Simplon\Db\DbInstance::MySQL('localhost', 'shcombr_appmanager', 'root', '');
 
             return $dbInstance;
